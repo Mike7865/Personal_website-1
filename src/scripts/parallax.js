3437e84a704e4f58ffb3e4abf7b9450b333098ea
Mike38 <mshar50055@gmail.com>
2019-11-06 17:03:06 +0300

    parallax.js updated, budda-parallax created

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,2 +1,19 @@
 const mountainsParallax = document.querySelector(".mountains-parallax");
 const mountainsLayer = mountainsParallax.children;
+
+function moveLayersDependsOnScroll(layers, wScroll) {
+  [...layers].forEach((layer) => {
+    if (layer.dataset.speed) {
+      const offset = -wScroll / layer.dataset.speed;
+      layer.style.transform = `translate3d(0, ${offset}px, 0)`;
+    }
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth < 768) {
+    return;
+  }
+  const wScroll = window.pageYOffset;
+  moveLayersDependsOnScroll(mountainsLayers, wScroll);
+});
